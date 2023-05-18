63df1a78e299b00a9795b2328437ffa714f2e46f
Alexis Creuzot <alexis.creuzot@gmail.com>
2023-05-18 12:32:49 -0600

    init

---
+++ docs/main.js
@@ -0,0 +1,16 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".show-once", {
+  reset: false });
+
+ScrollReveal().reveal(".fade-in", {
+  duration: 2000,
+  move: 0 });
+
+ScrollReveal().reveal(".scale-up", {
+  duration: 2000,
+  scale: 0.9 });
+
+ScrollReveal().reveal('.leave1', { distance: '200px', origin: 'left', delay: 500, duration: 1000, reset: false });
+ScrollReveal().reveal('.leave2', { distance: '200px', origin: 'right', delay: 800, duration: 1100, reset: false });
+ScrollReveal().reveal('.leave3', { distance: '200px', origin: 'left', delay: 1000, duration: 800, reset: false });
