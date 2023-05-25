c92a68f58334a96d765e25b6e3e4d257b8a99551
Alexis Creuzot <alexis.creuzot@gmail.com>
2023-05-25 11:52:42 -0600

    update 12 files

--- docs/main.js
+++ docs/main.js
@@ -1,20 +1,31 @@
 
-ScrollReveal({ reset: true });
-
-ScrollReveal().reveal(".show-once", {
-  reset: false });
-
-ScrollReveal().reveal(".fade-in", {
-  duration: 2000,
-  move: 0 });
-
-ScrollReveal().reveal(".scale-up", {
-  duration: 2000,
-  scale: 0.9 });
+ScrollReveal().reveal(".show-once", { reset: false });
+ScrollReveal().reveal(".scale-up", {duration: 2000,scale: 0.9 });
 
 ScrollReveal().reveal('.leave1', { distance: '200px', origin: 'left', delay: 500, duration: 1000, reset: false });
 ScrollReveal().reveal('.leave2', { distance: '200px', origin: 'right', delay: 800, duration: 1100, reset: false });
 ScrollReveal().reveal('.leave3', { distance: '200px', origin: 'left', delay: 1000, duration: 800, reset: false });
-
 ScrollReveal().reveal('.leave4', { distance: '200px', origin: 'left', delay: 500, duration: 1000, reset: false });
 ScrollReveal().reveal('.leave5', { distance: '200px', origin: 'right', delay: 800, duration: 1100, reset: false });
+
+
+jQuery(document).ready(function () {
+  jQuery("#nanogallery2").nanogallery2({
+    itemsBaseURL: './assets/images/',
+    viewerGallery: 'none',
+    thumbnailHeight:  300,
+    thumbnailWidth:   300,
+    thumbnailGutterWidth : 16,
+    thumbnailGutterHeight : 16,
+    viewerToolbar: {
+      standard:  'minimizeButton, label, fullscreenButton',
+      minimized: 'minimizeButton, label, fullscreenButton' 
+    },
+    viewerTools:    {
+      topLeft:   'label',
+      topRight:  'closeButton' 
+    },  
+    viewerTheme : { background: '#F00' },
+  });
+});
+
